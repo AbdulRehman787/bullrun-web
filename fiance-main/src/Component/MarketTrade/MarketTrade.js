a131c2d2d0882eae15bf7b07b937824d3e136312
AbdulRehman787 <a.rehman78766@gmail.com>
2023-04-18 12:15:51 +0500

    coment

--- fiance-main/src/Component/MarketTrade/MarketTrade.js
+++ fiance-main/src/Component/MarketTrade/MarketTrade.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import IbmApi from '../StockApi/IbmApi';
 import './style.css';
 import DowApi from './../StockApi/DowApi';
